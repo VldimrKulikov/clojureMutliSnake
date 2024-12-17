bfbe3451df0355dddfc88b4d097e65ddc3526263
German <amaliyabag@gmail.com>
2024-12-18 00:25:01 +0300

    feat: scores

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,14 +1,21 @@
-import {SnakeType} from "../types/snake.type.ts";
-import {CoordType} from "../types/coord.type.ts";
 import cn from 'classnames'
+import {GameState} from "../types/webSocketData.type.ts";
 
 interface IMap {
-    snakes: SnakeType[];
-    apple?: CoordType;
+    gameState: GameState | undefined;
 }
 
 const GRID_SIZE = 40;
-export const Map = ({snakes, apple}: IMap) => {
+export const Map = ({gameState}: IMap) => {
+    const apple = gameState?.food;
+    const players = Object.entries(gameState?.players || []);
+    const snakes = players
+        .filter(([, player]) => player.alive)
+        .map(([, player]) => ({
+            coords: player.snake || [],
+            color: player.color,
+        }));
+
     const cells = Array.from({length: GRID_SIZE}, (_, rowIndex) =>
         Array.from({length: GRID_SIZE}, (_, colIndex) => {
             return {x: colIndex, y: rowIndex, color: "white", isApple: false};
@@ -27,27 +34,25 @@ export const Map = ({snakes, apple}: IMap) => {
     })
 
     return (
-        <div className={"w-full h-screen flex items-center justify-center"}>
-            <div style={{
-                display: 'grid',
-                gridTemplateColumns: `repeat(${GRID_SIZE}, 1fr)`,
-                gridTemplateRows: `repeat(${GRID_SIZE}, 1fr)`,
-                width: '600px',
-                height: '600px'
-            }}>
-                {cells.flat().map((cell, index) => (
-                    <div
-                        className={cn(
-                            "w-full h-full border border-zinc-300",
-                            cell.isApple && "scale-105"
-                        )}
-                        key={index}
-                        style={{
-                            backgroundColor: cell.isApple ? 'red' : cell.color,
-                        }}
-                    />
-                ))}
-            </div>
+        <div style={{
+            display: 'grid',
+            gridTemplateColumns: `repeat(${GRID_SIZE}, 1fr)`,
+            gridTemplateRows: `repeat(${GRID_SIZE}, 1fr)`,
+            width: '600px',
+            height: '600px'
+        }}>
+            {cells.flat().map((cell, index) => (
+                <div
+                    className={cn(
+                        "w-full h-full border border-zinc-300",
+                        cell.isApple && "scale-105"
+                    )}
+                    key={index}
+                    style={{
+                        backgroundColor: cell.isApple ? 'red' : cell.color,
+                    }}
+                />
+            ))}
         </div>
     );
 }
---
+++ frontend/src/components/Score.tsx
@@ -0,0 +1,3 @@
+export const Score = ({score}: {score: number}) => {
+    return <div className={"text-white text-3xl"}>Ваш счет: {score}</div>
+}
